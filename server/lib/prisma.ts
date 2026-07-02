cc062c13de8bde16f458440a7c5dca9e861f75df
KhÁnh Hi <khanhvankhai281201@gmail.com>
2026-07-02 09:33:58 +0700

    Add docs

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,7 +1,29 @@
 import { PrismaClient } from "../generated/prisma/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const {
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+} = process.env;
+
+if (
+  !POSTGRES_USER ||
+  !POSTGRES_PASSWORD ||
+  !POSTGRES_DB ||
+  !POSTGRES_HOST ||
+  !POSTGRES_PORT
+) {
+  throw new Error(
+    "Missing required environment variables for database connection.",
+  );
+}
+
+const DATABASE_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
+
+const adapter = new PrismaPg({ connectionString: DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
